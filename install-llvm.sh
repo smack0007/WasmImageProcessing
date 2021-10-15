e93675e2ac2d45dee226cb84136ca7e87b4760cd
smack0007 <zachary.snow@gmail.com>
2021-10-15 17:24:34 +0200

    Ignore error on nothing to publish.

--- install-llvm.sh
+++ install-llvm.sh
@@ -2,8 +2,12 @@ wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
 sudo ./llvm.sh 13
 
-if [ -f "/usr/bin/clang" ]; then rm /usr/bin/clang fi
+if [ -f "/usr/bin/clang" ]; then
+  rm /usr/bin/clang
+fi
 sudo ln -s /usr/bin/clang-13 /usr/bin/clang
 
-if [ -f "/usr/bin/wasm-ld" ]; then rm /usr/bin/wasm-ld fi
+if [ -f "/usr/bin/wasm-ld" ]; then
+  rm /usr/bin/wasm-ld
+fi
 sudo ln -s /usr/bin/wasm-ld-13 /usr/bin/wasm-ld
