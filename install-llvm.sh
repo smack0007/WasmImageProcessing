ba4882aa5a06453c9e6c9c668881e0079cbce1b1
smack0007 <zachary.snow@gmail.com>
2021-10-15 17:21:47 +0200

    Remove symbolic links if they already exist.

--- install-llvm.sh
+++ install-llvm.sh
@@ -2,4 +2,8 @@ wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
 sudo ./llvm.sh 13
 
+if [ -f "/usr/bin/clang" ]; then rm /usr/bin/clang fi
 sudo ln -s /usr/bin/clang-13 /usr/bin/clang
+
+if [ -f "/usr/bin/wasm-ld" ]; then rm /usr/bin/wasm-ld fi
+sudo ln -s /usr/bin/wasm-ld-13 /usr/bin/wasm-ld
